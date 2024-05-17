0c522c9a9158d84aa4f337a329db991d7a13ceec
Thomas Cleary <thomcleary15@gmail.com>
2024-05-17 18:53:15 -0400

    Created a project using the React library and vite framework. Added navbar component.

---
+++ src/Navbar.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+
+const Navbar = () => {
+  // State to manage the navbar's visibility
+  const [nav, setNav] = useState(false);
+
+  // Toggle function to handle the navbar's display
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  // Array containing navigation items
+  const navItems = [
+    { id: 1, text: 'Home' },
+    { id: 2, text: 'About' },
+    { id: 3, text: 'Contact' },
+  ];
+
+  return (
+    <div className='bg-white flex justify-between shadow items-center h-24 mx-auto px-4 text-black'>
+
+      {/* Logo */}
+      <h1 className='w-full pl-8 text-3xl font-bold text-black'>Thomas Cleary</h1>
+
+      {/* Desktop Navigation */}
+      <ul className='hidden md:flex pr-8'>
+        {navItems.map(item => (
+          <li
+            key={item.id}
+            className='p-4 hover:bg-[#808080] rounded-xl m-2 cursor-pointer duration-300 hover:text-black'
+          >
+            {item.text}
+          </li>
+
+        ))}
+      
+      </ul>
+
+      {/* Mobile Navigation Icon */}
+      <div onClick={handleNav} className='block md:hidden pr-8'>
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+
+      {/* Mobile Navigation Menu */}
+      <ul
+        className={
+          nav
+            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-white ease-in-out duration-500'
+            : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+        }
+      >
+        {/* Mobile Logo */}
+        <h1 className='w-full text-3xl font-bold text-black m-4'>Thomas Cleary</h1>
+
+        {/* Mobile Navigation Items */}
+        {navItems.map(item => (
+          <li
+            key={item.id}
+            className='p-4 border-b rounded-none hover:bg-gray duration-300 hover:text-black cursor-pointer border-gray-600'
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
